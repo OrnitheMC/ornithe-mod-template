d92c01a5c6c110cf04487b35b673ff3b30217cfa
Space Walker <spacedoesrs@gmail.com>
2025-08-13 18:47:55 +0200

    switch to log4j logger

--- client/src/main/java/com/example/example_mod/ExampleMod.java
+++ client/src/main/java/com/example/example_mod/ExampleMod.java
@@ -1,7 +1,7 @@
 package com.example.example_mod;
 
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import net.ornithemc.osl.entrypoints.api.ModInitializer;
 
@@ -10,7 +10,7 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogManager().getLogger("Example Mod");
+	public static final Logger LOGGER = LogManager.getLogger("Example Mod");
 
 	@Override
 	public void init() {
